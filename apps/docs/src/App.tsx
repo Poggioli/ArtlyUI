fae5aeb26fa8c092fee9946f71d086ade24a0076
Poggioli <joaopogiolli@gmail.com>
2023-01-19 08:32:22 -0300

    chore: add docs project

---
+++ apps/docs/src/App.tsx
@@ -0,0 +1,11 @@
+import { deepPurpleAndAmberTheme } from "@artly-ui/core";
+
+function App() {
+  return (
+    <div className={`App ${deepPurpleAndAmberTheme.className}`}>
+      <span>blabla</span>
+    </div>
+  );
+}
+
+export default App;
