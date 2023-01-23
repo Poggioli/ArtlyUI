07035f3710f25c22bb52993a9dbe87798d060531
Poggioli <joaopogiolli@gmail.com>
2023-01-22 22:32:19 -0300

    chore: add button storie

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -1,10 +1,105 @@
-import { deepPurpleAndAmberTheme } from "@artly-ui/core";
+import { Button } from "@artly-ui/button";
+import { deepPurpleAndAmberTheme, indigoPinkTheme } from "@artly-ui/core";
+import { HeartFilledIcon } from "@radix-ui/react-icons";
 
 function App() {
   return (
-    <div className={`App ${deepPurpleAndAmberTheme.className}`}>
-      <span>blabla</span>
-    </div>
+    <>
+      <div className={`App ${indigoPinkTheme.className}`}>
+        <Button color="unstyled">
+          Test <HeartFilledIcon />
+        </Button>
+        <Button color="primary">
+          Test <HeartFilledIcon />
+        </Button>
+        <Button color="secondary">
+          Test <HeartFilledIcon />
+        </Button>
+        <Button color="accent">
+          Test <HeartFilledIcon />
+        </Button>
+        <Button color="error">
+          Test <HeartFilledIcon />
+        </Button>
+        <Button color="unstyled" shape="flat">
+          Test
+        </Button>
+        <Button color="primary" shape="flat">
+          Test
+        </Button>
+        <Button color="secondary" shape="flat">
+          Test
+        </Button>
+        <Button color="accent" shape="flat">
+          Test
+        </Button>
+        <Button color="error" shape="flat">
+          Test
+        </Button>
+        <Button color="unstyled" as="a" href="https://www.google.com">
+          Test
+        </Button>
+        <Button color="primary" as="a" href="https://www.google.com">
+          Test
+        </Button>
+        <Button color="secondary" as="a" href="https://www.google.com">
+          Test
+        </Button>
+        <Button color="accent" as="a" href="https://www.google.com">
+          Test
+        </Button>
+        <Button color="error" as="a" href="https://www.google.com">
+          Test
+        </Button>
+      </div>
+      <div className={`App ${deepPurpleAndAmberTheme.className}`}>
+        <Button color="unstyled">
+          Test <HeartFilledIcon />
+        </Button>
+        <Button color="primary">
+          Test <HeartFilledIcon />
+        </Button>
+        <Button color="secondary">
+          Test <HeartFilledIcon />
+        </Button>
+        <Button color="accent">
+          Test <HeartFilledIcon />
+        </Button>
+        <Button color="error">
+          Test <HeartFilledIcon />
+        </Button>
+        <Button color="unstyled" shape="flat">
+          Test
+        </Button>
+        <Button color="primary" shape="flat">
+          Test
+        </Button>
+        <Button color="secondary" shape="flat">
+          Test
+        </Button>
+        <Button color="accent" shape="flat">
+          Test
+        </Button>
+        <Button color="error" shape="flat">
+          Test
+        </Button>
+        <Button color="unstyled" as="a" href="https://www.google.com">
+          Test
+        </Button>
+        <Button color="primary" as="a" href="https://www.google.com">
+          Test
+        </Button>
+        <Button color="secondary" as="a" href="https://www.google.com">
+          Test
+        </Button>
+        <Button color="accent" as="a" href="https://www.google.com">
+          Test
+        </Button>
+        <Button color="error" as="a" href="https://www.google.com">
+          Test
+        </Button>
+      </div>
+    </>
   );
 }
 
---
+++ apps/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@artly-ui/button";
+import { HeartFilledIcon, PlusIcon } from "@radix-ui/react-icons";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+const story: ComponentMeta<typeof Button> = {
+  title: "Core components/Button",
+  component: Button,
+  argTypes: {
+    color: {
+      control: {
+        type: "select",
+      },
+      options: ["unstyled", "primary", "secondary", "accent", "error"],
+    },
+    shape: {
+      control: {
+        type: "select",
+      },
+      options: ["basic", "stroked", "flat", "icon"],
+    },
+    iconPosition: {
+      control: {
+        type: "select",
+      },
+      options: ["left", "right", "none"],
+    },
+    disabled: {
+      type: "boolean",
+      defaultValue: false,
+    },
+  },
+};
+
+const Template: ComponentStory<typeof Button> = ({ ...args }) => {
+  if (args.shape === "icon") {
+    return (
+      <Button {...args} aria-label="heart icon">
+        <HeartFilledIcon />
+      </Button>
+    );
+  }
+
+  if (args.iconPosition !== "none") {
+    return (
+      <Button {...args} icon={<PlusIcon />}>
+        Button
+      </Button>
+    );
+  }
+
+  return <Button {...args}>Button</Button>;
+};
+
+export const Default = Template.bind({});
+export default story;
