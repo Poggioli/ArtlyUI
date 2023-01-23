97470f3d1bb54557ef60356608ee8a030276ddfe
Poggioli <joaopogiolli@gmail.com>
2023-01-22 22:22:38 -0300

    chore: add color tokens

--- packages/artly-ui-themes/src/themes/deep-purple-and-amber.ts
+++ packages/artly-ui-themes/src/themes/deep-purple-and-amber.ts
@@ -2,4 +2,5 @@ import { generateColorTokens } from "../configs/generate-color-tokens";
 
 export const DeepPurpleAndAmberTheme = {
   ...generateColorTokens({}),
+  "button--flat--secondary--text-normal": "$black-300",
 };
