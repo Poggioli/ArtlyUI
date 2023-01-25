a439c6f6be649e2e0b486afd70378c969a9d82c8
Poggioli <joaopogiolli@gmail.com>
2023-01-24 21:34:00 -0300

    fix: Text mount test

--- packages/artly-ui-text/src/Text.spec.tsx
+++ packages/artly-ui-text/src/Text.spec.tsx
@@ -5,14 +5,16 @@ describe("Core component / Text", () => {
   it(`GIVEN a Text component
       WHEN user dont set the color props
       SHOULD use $$text-high-contrast token`, () => {
-    const rendered = render(<Text />);
+    const rendered = render(<Text>Text component</Text>);
     expect(rendered.container).toBeTruthy();
   });
 
   it(`GIVEN a Text component
       WHEN user set the color props
       SHOULD use the color passed`, () => {
-    const rendered = render(<Text color="$text-low-contrast" />);
+    const rendered = render(
+      <Text color="$text-low-contrast">Text component</Text>
+    );
     expect(rendered.container).toBeTruthy();
   });
 });
