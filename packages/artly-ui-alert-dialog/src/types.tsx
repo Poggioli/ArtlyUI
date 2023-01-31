31306b336e2dfe70a6882f5310f787fb1fa39515
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-01-31 19:44:03 -0300

    chore: add types to alert-dialog package

--- packages/artly-ui-alert-dialog/src/types.tsx
+++ packages/artly-ui-alert-dialog/src/types.tsx
@@ -1,5 +1,19 @@
-import { ArtlyVariants, HTMLArtlyProps } from "@artly-ui/core";
-import { StyledAlertDialog } from "./styles";
+import * as AlertDialogPrimitives from "@radix-ui/react-alert-dialog";
 
-export type AlertDialogProps = HTMLArtlyProps<typeof StyledAlertDialog> &
-  ArtlyVariants<typeof StyledAlertDialog>;
+export type AlertDialogProps = AlertDialogPrimitives.AlertDialogProps;
+
+export type AlertDialogTriggerProps = AlertDialogPrimitives.AlertDialogProps;
+
+export type AlertDialogCancelProps = AlertDialogPrimitives.AlertDialogProps;
+
+export type AlertDialogActionProps = AlertDialogPrimitives.AlertDialogProps;
+
+export type AlertDialogDescriptionProps =
+  AlertDialogPrimitives.AlertDialogProps;
+
+export type AlertDialogTitleProps = AlertDialogPrimitives.AlertDialogProps;
+
+export type AlertDialogContentProps =
+  AlertDialogPrimitives.AlertDialogPortalProps &
+    AlertDialogPrimitives.AlertDialogContentProps &
+    AlertDialogPrimitives.AlertDialogOverlayProps;
