2f801c86ed34f679539477336986e6856a255bb2
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-02-01 20:19:44 -0300

    chore: add style components

--- packages/artly-ui-collapsible/src/styles.tsx
+++ packages/artly-ui-collapsible/src/styles.tsx
@@ -1,5 +1,43 @@
-import { styled } from "@artly-ui/core";
+import { keyframes, styled } from "@artly-ui/core";
+import * as CollapsiblePrimitives from "@radix-ui/react-collapsible";
 
-const StyledCollapsible: any = styled("div");
+const slideDown = keyframes({
+  from: {
+    height: 0,
+  },
+  to: {
+    height: "var(--radix-collapsible-content-height)",
+  },
+});
 
-export { StyledCollapsible };
+const slideUp = keyframes({
+  from: {
+    height: "var(--radix-collapsible-content-height)",
+  },
+  to: {
+    height: 0,
+  },
+});
+
+const StyledCollapsibleRoot = CollapsiblePrimitives.Root;
+
+const StyledCollapsibleContent = styled(CollapsiblePrimitives.Content, {
+  overflow: "hidden",
+
+  '&[data-state="open"]': {
+    animation: `${slideDown} 200ms cubic-bezier(0.16, 1, 0.3, 1)`,
+  },
+
+  '&[data-state="closed"]': {
+    animation: `${slideUp} 200ms cubic-bezier(0.16, 1, 0.3, 1)`,
+  },
+});
+
+const StyledCollapsibleTrigger = CollapsiblePrimitives.Trigger;
+
+export {
+    StyledCollapsibleRoot,
+    StyledCollapsibleContent,
+    StyledCollapsibleTrigger,
+};
+
