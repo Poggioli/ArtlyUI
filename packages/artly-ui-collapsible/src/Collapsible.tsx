b2e6c3c6065886f78d8f5a51dd17cc38a222c684
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-02-01 20:19:57 -0300

    chore: add components

--- packages/artly-ui-collapsible/src/Collapsible.tsx
+++ packages/artly-ui-collapsible/src/Collapsible.tsx
@@ -1,11 +1,38 @@
 import { forwardRef } from "@artly-ui/core";
-import { StyledCollapsible } from "./styles";
-import { CollapsibleProps } from "./types";
+import {
+  StyledCollapsibleContent,
+  StyledCollapsibleRoot,
+  StyledCollapsibleTrigger
+} from "./styles";
+import { CollapsibleTriggerProps } from "./types";
 
-const Collapsible = forwardRef<typeof StyledCollapsible, CollapsibleProps>(
-  ({ ...props }, forwardedRef) => {
-    return <StyledCollapsible {...props} ref={forwardedRef} />
+// ------------------------------------------- Collapsible Trigger ------------------------------------------- //
+
+const CollapsibleTrigger = forwardRef<
+  typeof StyledCollapsibleTrigger,
+  CollapsibleTriggerProps
+>(({ asChild, children, ...props }, forwardedRef) => {
+  if (asChild === undefined) {
+    return (
+      <StyledCollapsibleTrigger {...props} ref={forwardedRef} asChild>
+        {children}
+      </StyledCollapsibleTrigger>
+    );
   }
-);
 
-export { Collapsible };
+  return (
+    <StyledCollapsibleTrigger {...props} ref={forwardedRef} asChild={asChild}>
+      {children}
+    </StyledCollapsibleTrigger>
+  );
+});
+
+// ------------------------------------------- Collapsible Root ------------------------------------------- //
+
+const Collapsible = StyledCollapsibleRoot;
+
+// ------------------------------------------- Collapsible Content ------------------------------------------- //
+
+const CollapsibleContent = StyledCollapsibleContent;
+
+export { CollapsibleTrigger, Collapsible, CollapsibleContent };
