481d13bbf31a5f60ce703b22346f9a3ceb7b99f3
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-02-01 21:18:56 -0300

    test: add tests to collapsible component

--- packages/artly-ui-collapsible/src/Collapsible.spec.tsx
+++ packages/artly-ui-collapsible/src/Collapsible.spec.tsx
@@ -1,11 +1,64 @@
 import { render } from "@testing-library/react";
-import { Collapsible } from "./Collapsible";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger
+} from "./Collapsible";
 
-describe("Core component / Collapsible", () => {
+describe("Data display component / Collapsible", () => {
+  describe("Collapsible trigger", () => {
     it(`GIVEN a Collapsible component
-    WHEN render component
-    SHOULD occurs render normaly`, () => {
-    const rendered = render(<Collapsible />);
-    expect(rendered.container).toBeTruthy();
+        WHEN asChild props is NOT setted in CollapsibleTrigger
+        SHOULD render a button`, () => {
+      const rendered = render(
+        <Collapsible>
+          <CollapsibleTrigger>
+            <button>Trigger</button>
+          </CollapsibleTrigger>
+          <CollapsibleContent>Any Content</CollapsibleContent>
+        </Collapsible>
+      );
+
+      const triggerButton = rendered.getByText("Trigger", {
+        selector: 'button[data-state="closed"]',
+      });
+      expect(triggerButton).toBeVisible();
+    });
+
+    it(`GIVEN a Collapsible component
+        WHEN asChild props is setted to false in CollapsibleTrigger
+        SHOULD render StyledCollapsibleTrigger`, () => {
+      const rendered = render(
+        <Collapsible>
+          <CollapsibleTrigger asChild={false}>
+            <button>Trigger</button>
+          </CollapsibleTrigger>
+          <CollapsibleContent>Any Content</CollapsibleContent>
+        </Collapsible>
+      );
+
+      const triggerButton = rendered.getByText("Trigger", {
+        selector: 'button[data-state="closed"] button',
+      });
+      expect(triggerButton).toBeVisible();
+    });
+
+    it(`GIVEN a Collapsible component
+        WHEN asChild props is setted to true in CollapsibleTrigger
+        SHOULD render the children component`, () => {
+      const rendered = render(
+        <Collapsible>
+          <CollapsibleTrigger asChild>
+            <button>Trigger</button>
+          </CollapsibleTrigger>
+          <CollapsibleContent>Any Content</CollapsibleContent>
+        </Collapsible>
+      );
+
+      const triggerButton = rendered.getByText("Trigger", {
+        selector: 'button[data-state="closed"]',
+      });
+      expect(triggerButton).toBeVisible();
+    });
   });
 });
