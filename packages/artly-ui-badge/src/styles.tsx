844e9e103c38239a7b93130e3a649ace53e07e7c
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-02-07 14:34:34 -0300

    feat: add styled components

--- packages/artly-ui-badge/src/styles.tsx
+++ packages/artly-ui-badge/src/styles.tsx
@@ -1,5 +1,61 @@
 import { styled } from "@artly-ui/core";
+import { Flex } from "@artly-ui/flex";
 
-const StyledBadge: any = styled("div");
+const StyledBadgeContainer: any = styled(Flex, {
+  position: "relative",
+});
 
-export { StyledBadge };
+const StyledBadge: any = styled(Flex, {
+  height: "$5",
+  minWidth: "$5",
+  position: "absolute",
+  backgroundColor: "$$backgroundColor",
+
+  "&, *": {
+    textColor: "$$textColor",
+  },
+
+  variants: {
+    horizontalPosition: {
+      left: {
+        left: 0,
+      },
+      center: {},
+      right: {
+        right: 0,
+      },
+    },
+    verticalPosition: {
+      top: {
+        top: 0,
+      },
+      center: {},
+      bottom: {
+        bottom: 0,
+      },
+    },
+    color: {
+      primary: {},
+      secondary: {},
+      unstyled: {},
+      accent: {},
+      error: {},
+    },
+    dot: {
+      true: {
+        height: "$3",
+        minWidth: "initial",
+        width: "$3",
+      },
+    },
+  },
+
+  defaultVariants: {
+    horizontalPosition: "right",
+    verticalPosition: "top",
+    color: "primary",
+    dot: false,
+  },
+});
+
+export { StyledBadge, StyledBadgeContainer };
