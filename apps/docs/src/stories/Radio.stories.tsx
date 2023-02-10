c83a75c54323112bc94f0beca75e813841655afa
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-02-10 13:53:31 -0300

    feat: create radio component

--- apps/docs/src/stories/Radio.stories.tsx
+++ apps/docs/src/stories/Radio.stories.tsx
@@ -1,6 +1,6 @@
-import {
-  Radio
-} from "@artly-ui/radio";
+import { Flex } from "@artly-ui/flex";
+import { Heading } from "@artly-ui/heading";
+import { Radio, RadioContainer, RadioItem, RadioLabel } from "@artly-ui/radio";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 const story: ComponentMeta<typeof Radio> = {
@@ -22,7 +22,42 @@ const story: ComponentMeta<typeof Radio> = {
 };
 
 const Template: ComponentStory<typeof Radio> = ({ ...args }) => {
-  return (<Radio {...args} id="radio-1" />);
+  return (
+    <Flex direction="column" justify="space-evenly" css={{ height: "100%" }}>
+      <Flex direction="column" gap="$5" align="center">
+        <Heading level={5}>Without label</Heading>
+        <Radio {...args}>
+          <Flex gap="$5" align="center">
+            <RadioItem value="radio-1" />
+            <RadioItem value="radio-2" />
+          </Flex>
+        </Radio>
+      </Flex>
+      <Flex direction="column" gap="$5" align="center">
+        <Heading level={5}>With label</Heading>
+        <Radio {...args}>
+          <Flex gap="$5" align="center">
+            <RadioContainer>
+              <RadioItem id="daily" value="daily" />
+              <RadioLabel htmlFor="daily">Daily</RadioLabel>
+            </RadioContainer>
+            <RadioContainer>
+              <RadioItem id="4-hours" value="4-hours" />
+              <RadioLabel htmlFor="4-hours">Every 4 hours</RadioLabel>
+            </RadioContainer>
+            <RadioContainer>
+              <RadioItem id="8-hours" value="8-hours" />
+              <RadioLabel htmlFor="8-hours">Every 8 hours</RadioLabel>
+            </RadioContainer>
+            <RadioContainer>
+              <RadioItem id="12-hours" value="12-hours" />
+              <RadioLabel htmlFor="12-hours">Every 12 hours</RadioLabel>
+            </RadioContainer>
+          </Flex>
+        </Radio>
+      </Flex>
+    </Flex>
+  );
 };
 
 export const Default = Template.bind({});
--- packages/artly-ui-radio/src/Radio.tsx
+++ packages/artly-ui-radio/src/Radio.tsx
@@ -1,11 +1,70 @@
 import { forwardRef } from "@artly-ui/core";
-import { StyledRadio } from "./styles";
-import { RadioProps } from "./types";
+import {
+  StyledRadioGroupContainer,
+  StyledRadioGroupIndicator,
+  StyledRadioGroupItem,
+  StyledRadioGroupLabel,
+  StyledRadioGroupRoot,
+} from "./styles";
+import {
+  RadioContainerProps,
+  RadioItemProps,
+  RadioLabelProps,
+  RadioProps,
+} from "./types";
 
-const Radio = forwardRef<typeof StyledRadio, RadioProps>(
-  ({ ...props }, forwardedRef) => {
-    return <StyledRadio {...props} ref={forwardedRef} />;
+// ------------------------------------------- RadioGroup Root ------------------------------------------- //
+
+const Radio = forwardRef<typeof StyledRadioGroupRoot, RadioProps>(
+  ({ children, ...props }, forwardedRef) => {
+    return (
+      <StyledRadioGroupRoot {...props} ref={forwardedRef}>
+        {children}
+      </StyledRadioGroupRoot>
+    );
   }
 );
 
-export { Radio };
+// ------------------------------------------- Radio Item ------------------------------------------- //
+
+const RadioItem = forwardRef<typeof StyledRadioGroupItem, RadioItemProps>(
+  ({ ...props }, forwardedRef) => {
+    return (
+      <StyledRadioGroupItem {...props} ref={forwardedRef}>
+        <StyledRadioGroupIndicator />
+      </StyledRadioGroupItem>
+    );
+  }
+);
+
+// ------------------------------------------- Radio Container ------------------------------------------- //
+
+const RadioContainer = forwardRef<
+  typeof StyledRadioGroupContainer,
+  RadioContainerProps
+>(({ children, ...props }, forwardedRef) => {
+  return (
+    <StyledRadioGroupContainer
+      {...props}
+      ref={forwardedRef}
+      align="center"
+      justify="space-between"
+      gap="$3"
+      wrap="nowrap"
+    >
+      {children}
+    </StyledRadioGroupContainer>
+  );
+});
+
+// ------------------------------------------- Radio Label ------------------------------------------- //
+
+const RadioLabel = forwardRef<typeof StyledRadioGroupLabel, RadioLabelProps>(
+  ({ ...props }, forwardedRef) => {
+    return (
+      <StyledRadioGroupLabel {...props} ref={forwardedRef} variant="body1" />
+    );
+  }
+);
+
+export { Radio, RadioItem, RadioContainer, RadioLabel };
